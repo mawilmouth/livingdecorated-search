66fdbedc2632796ed9dd09ef1c197249a3f36e29
Michael Wilmouth <wilmouthworks@gmail.com>
2022-01-16 22:13:16 -0500

    Add published scope to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
+  POST_TYPE = 'post'.freeze
+  PAGE_TYPE = 'page'.freeze
+
+  PUBLISHED_STATUS = 'published'.freeze
+  UNPUBLISHED_STATUS = 'draft'.freeze
+
+  scope :published, -> { where(status: PUBLISHED_STATUS) }
+  
   self.inheritance_column = nil
 end
