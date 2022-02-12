1b282da8efe41fe181fa1c76cfe54422bf569292
Michael Wilmouth <wilmouthworks@gmail.com>
2022-02-12 18:18:43 -0500

    Add Post#posts and Post#order_by_published scopes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,5 +11,28 @@ RSpec.describe Post, type: :model do
         expect(described_class.published).to match_array [post_one, post_three]
       end
     end
+
+    describe '#posts' do
+      let!(:post_one) { create(:post, :as_page) }
+      let!(:post_two) { create(:post) }
+      let!(:post_three) { create(:post) }
+
+      it 'returns the expected posts' do
+        expect(described_class.posts).to match_array [post_two, post_three]
+      end
+    end
+
+    describe '#order_by_published' do
+      let!(:post_one) { create(:post, published_at: Time.current - 1.day) }
+      let!(:post_two) { create(:post, published_at: Time.current) }
+      let!(:post_three) { create(:post, published_at: Time.current - 2.days) }
+
+      it 'returns the posts in the expected order' do
+        result = described_class.order_by_published
+        expect(result[0]).to eq post_two
+        expect(result[1]).to eq post_one
+        expect(result[2]).to eq post_three
+      end
+    end
   end
 end
